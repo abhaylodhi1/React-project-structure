8ea754bd69a5cad001aec4712f668244318f4868
Abhay_Lodhi <abhaylodhi2001@gmail.com>
2025-02-28 15:58:28 +0530

    feat(blog): add comment section

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 const queryClient = new QueryClient();
 
+/// test eslint
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
